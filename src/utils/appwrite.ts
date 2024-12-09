8e82af14488a81d6081f5e86507b3d18f55b7704
Eric Sison <ericsison.dev@gmail.com>
2024-12-09 13:55:27 +0800

    set env var for appwrite endpoint and project_id

--- src/utils/appwrite.ts
+++ src/utils/appwrite.ts
@@ -3,8 +3,8 @@ import { Client, Account } from "appwrite";
 export const client = new Client();
 
 client
-  .setEndpoint("http://172.20.110.100:82/v1")
-  .setProject("67565b5c0006e1006e06");
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_API_ENDPOINT!)
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID!);
 
 export const account = new Account(client);
 
